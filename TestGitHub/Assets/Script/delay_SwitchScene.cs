52f785cf4edbfce7a0ef893ef790e141faf101eb
AutumnLee172 <78860600+AutumnLee172@users.noreply.github.com>
2021-10-03 13:09:56 +0800

    1

--- TestGitHub/Assets/Script/delay_SwitchScene.cs
+++ TestGitHub/Assets/Script/delay_SwitchScene.cs
@@ -5,15 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class delay_SwitchScene : MonoBehaviour
 {
+    public string sceneToLoad;
+    public int delayTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("ChangeScene", delayTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
